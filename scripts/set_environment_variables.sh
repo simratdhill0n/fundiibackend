f95eaac3694a11d479f29a5ccb7ab1fa8deb44cb
simrat dhillon <simratpalsinghdhillon@gmail.com>
2023-06-12 12:13:21 -0400

    [FIX] test shell

--- scripts/set_environment_variables.sh
+++ scripts/set_environment_variables.sh
@@ -3,7 +3,10 @@ export SECRET_KEY="django-insecure-#y_kh73zws_w%5&_+i(=-p75(nw#o0)d#+v^_p**^&3vv
 export JWT_SECRET_KEY="d8d8dd6c8003dd637b3903a18fea0119c4f1900ab51260c9339b6ff26c6a032f"
 export PSQL_DB_NAME="dev_database"
 export PSQL_DB_USER="postgres"
-export PSQL_DB_PASSWD="12345678"
+export PSQL_DB_PASSWD="1234567"
 export PSQL_HOST="database-development.ca7zrnuaeaa4.us-east-1.rds.amazonaws.com" 
 export PSQL_PORT="5432"
 export PSQL_ENGINE="django.db.backends.postgresql"
+
+sudo service gunicorn restart
+sudo service nginx restart
